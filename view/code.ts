7c2c57b226aee0299bef150cd3244722c0a445e3
Adam Vesecký <vesecky.adam@gmail.com>
2022-10-09 14:45:43 +0200

    Add some twists

--- view/code.ts
+++ view/code.ts
@@ -12,7 +12,7 @@ const initRelax = () => {
     rellax.destroy();
     const width = window.innerWidth;
     
-    const elements = ['bgr_x4', 'bgr_x5', 'bgr_x6', 'bgr_x7']
+    const elements = ['bgr_x4', 'bgr_x5', 'bgr_x6', 'bgr_x7', 'bgr_x8']
     const ratio = IMG_WIDTH / width;
     // 0.1 means 10% from the bottom .. 0.5 as we want the image show up 50% above the border
     const bottom = 0.1 * window.innerHeight - 0.5 * (IMG_HEIGHT / ratio);
@@ -21,7 +21,10 @@ const initRelax = () => {
         const element = document.getElementById(el);    
         element?.style.setProperty('bottom', `${bottom}px`);
 
-        const speed = Math.floor((index + 1) / (ratio) * 3);
+        let speed = ((index + 1) / (ratio) * 3);
+        if(el === 'bgr_x7') {
+            speed *= (3 * window.innerWidth / window.innerHeight);
+        }
         element?.setAttribute('data-rellax-speed', `${speed}`);
     });
 
