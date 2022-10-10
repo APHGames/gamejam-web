dc9b5c2df649b65408853821b42d095b866f9da1
Adam Vesecký <vesecky.adam@gmail.com>
2022-10-10 20:17:46 +0200

    Final polishing

--- view/code.ts
+++ view/code.ts
@@ -3,15 +3,15 @@ import Rellax from 'rellax';
 const IMG_WIDTH = 2286;
 const IMG_HEIGHT = 2492;
 
-let rellax = new Rellax('.rellax', {
-    vertical: true,
-    horizontal: false
-});
+let rellax: any = null;
 
 const initRelax = () => {
-    rellax.destroy();
+    if(rellax) {
+        rellax.destroy();
+    }   
     const width = window.innerWidth;
     
+	// elements that, without this script, would levitate every fucking where
     const elements = ['bgr_x4', 'bgr_x5', 'bgr_x6', 'bgr_x7', 'bgr_x8']
     const ratio = IMG_WIDTH / width;
     // 0.1 means 10% from the bottom .. 0.5 as we want the image show up 50% above the border
@@ -23,11 +23,17 @@ const initRelax = () => {
 
         let speed = ((index + 1) / (ratio) * 3);
         if(el === 'bgr_x7') {
+			// This element is special, because it displays some text
+			// why 3? Because... 3 does exactly what I want it to do
             speed *= (3 * window.innerWidth / window.innerHeight);
+        }
+        if(el === 'bgr_x8') {
+            speed /= 1.5;
         }
         element?.setAttribute('data-rellax-speed', `${speed}`);
     });
 
+	// initialize the god damn library... freaking useless undocumented piece of trash
     rellax = new Rellax('.rellax', {
         vertical: true,
         horizontal: false
