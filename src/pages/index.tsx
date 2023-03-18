9e51225db8de59fde3a0120b616e7f6fbe61708a
Adam Vesecký <vesecky.adam@gmail.com>
2023-03-18 20:00:00 +0100

    Add parallax

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,21 @@ import LoadingSpinner from '../components/Loading';
 import sectionStyles from '../css/section.module.scss';
 import titleStyles from '../css/title.module.scss';
 import contentStyles from '../css/content.module.scss';
+import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax";
+
+
+import bgr1 from '../../static/img/pages/index/parallax/bgr1.png';
+import bgr1x from '../../static/img/pages/index/parallax/bgr1x.png';
+import bgr2 from '../../static/img/pages/index/parallax/bgr2.png';
+import bgr3 from '../../static/img/pages/index/parallax/bgr3.png';
+import bgr4 from '../../static/img/pages/index/parallax/bgr4.png';
+import bgr4x from '../../static/img/pages/index/parallax/bgr4x.png';
+import bgr5 from '../../static/img/pages/index/parallax/bgr5.png';
+import bgr6 from '../../static/img/pages/index/parallax/bgr6.png';
+import bgr7 from '../../static/img/pages/index/parallax/bgr7.png';
 
 const Title = () => (
-	<section
+	/*<section
 		className={clsx(sectionStyles.section)}
 	>
 		<div className={clsx(sectionStyles['section--inner'], titleStyles.title__section)}>
@@ -21,6 +33,111 @@ const Title = () => (
 					<h2>{translate({ message: 'index.title' })}</h2>
 				</div>
 			</div>
+		</div>
+	</section>*/
+	<section className={clsx(sectionStyles['section--parallax'])}>
+		<ParallaxProvider>
+			<ParallaxBanner
+				className={clsx(sectionStyles['section--banner'])}
+				layers={[
+					{
+						image: bgr7,
+						translateY: [0, 0],
+						style: {
+							backgroundSize: 'contain',
+							backgroundPosition: 'bottom center',
+						},
+						shouldAlwaysCompleteAnimation: true,
+						expanded: false,
+					},
+					{
+						image: bgr6,
+						translateY: [40, 0],
+						shouldAlwaysCompleteAnimation: false,
+						style: {
+							backgroundSize: 'contain',
+							backgroundPosition: 'bottom center',
+						},
+						expanded: false,
+					},
+					{
+						image: bgr5,
+						translateY: [15, 0],
+						style: {
+							backgroundSize: 'contain',
+							backgroundPosition: 'bottom right',
+						},
+						shouldAlwaysCompleteAnimation: true,
+						expanded: false,
+					},
+					{
+						image: bgr4,
+						translateY: [12, 0],
+						style: {
+							backgroundSize: 'contain',
+							backgroundPosition: 'top left',
+						},
+						shouldAlwaysCompleteAnimation: false,
+						expanded: false,
+					},
+					{
+						image: bgr4x,
+						translateY: [15, 0],
+						style: {
+							backgroundSize: 'contain',
+							backgroundPosition: 'top right',
+						},
+						shouldAlwaysCompleteAnimation: true,
+						expanded: false,
+					},
+					{
+						image: bgr3,
+						translateY: [20, 0],
+						style: {
+							backgroundSize: 'contain',
+							backgroundPosition: 'bottom center',
+						},
+						shouldAlwaysCompleteAnimation: true,
+						expanded: false,
+					},
+					{
+						image: bgr2,
+						translateY: [-10, -110],
+						style: {
+							backgroundSize: 'contain',
+							backgroundPosition: 'bottom center',
+						},
+						shouldAlwaysCompleteAnimation: true,
+						expanded: false,
+					},
+					{
+						image: bgr1,
+						translateY: [0, 200],
+						style: {
+							backgroundSize: 'contain',
+							backgroundPosition: 'bottom center',
+						},
+						shouldAlwaysCompleteAnimation: true,
+						expanded: false,
+					},
+					{
+						image: bgr1x,
+						translateY: [20, 150],
+						style: {
+							backgroundSize: 'contain',
+							backgroundPosition: 'bottom center',
+						},
+						shouldAlwaysCompleteAnimation: true,
+						expanded: false,
+					},
+				]}
+			>
+				
+			</ParallaxBanner>
+		</ParallaxProvider>
+		<div className={clsx(sectionStyles['section--overlay'])}>asd</div>
+		<div className={clsx(sectionStyles['section--title'])}>
+			<div>GameJam</div>
 		</div>
 	</section>
 );
@@ -73,6 +190,11 @@ const Home = () => {
 			</DocusaurusHead>
 			<Title />
 			<AboutComponent />
+			<News />
+			<News />
+			<News />
+			<News />
+			<News />
 			<News />
 		</Layout>
 	);
